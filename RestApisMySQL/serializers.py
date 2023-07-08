d9d5596efbf467e95528acae142572de09875fdd
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-07-08 16:21:35 -0300

    criação do bd e migraçao

---
+++ RestApisMySQL/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers 
+from appRestApi.models import PosterDados
+ 
+ 
+class PosterDadosSerializer(serializers.ModelSerializer):
+ 
+    class Meta:
+        model = PosterDados
+        fields = ('id',
+                  'autor',
+                  'titulo',
+                  'descricao',
+                  'datapublicacao')
---
+++ appRestApi/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class PosterDados(models.Model):
+    autor = models.CharField(max_length=50)
+    titulo = models.CharField(max_length=70)
+    descricao = models.CharField(max_length=200)
+    datapublicacao = models.DateField(auto_now=True)
+                              
